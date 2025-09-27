937a85b725ac003c99792af47aca5ea6fd94f570
Cheeckyio <kwabenaafoa199@gmail.com>
2025-09-27 20:56:27 +0000

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,24 @@
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter operation ('add', 'subtract', 'multiply', 'divide'): ").strip().lower()
+
+    
+    if operation == 'add':
+        return num1 + num2
+    
+    elif operation == 'subtract':
+        return num1 - num2
+    
+    elif operation == 'multiply':
+        return num1 * num2
+    
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        return num1 / num2
+    
+    else:
+        return f"Error: Invalid operation '{operation}'. Valid operations are 'add', 'subtract', 'multiply', 'divide'"
